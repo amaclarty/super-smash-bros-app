ed71ee056ee7b3989dd4365b8f395db2cef38981
amaclarty <100883044+amaclarty@users.noreply.github.com>
2022-07-20 11:46:50 -0400

    added flexbox and transition

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@
 import './App.css';
 
 function App() {
+  const fighters = ['Wario', 'Yoshi', 'Ganondorf', 'Kirby', 'Samus', 'Sheik', 'Link', 'Mario']
   return (
     <div className="App">
-      <div className="fighters-grid">
        <h1>Fighters</h1>
+       <div className="fighters-grid">
+       {
+        fighters.map((element, index) => {
+          return(
+            <div>
+              <img src={`https://www.smashbros.com/assets_v2/img/fighter/thumb_a/${element.toLowerCase()}.png`} />
+            </div>
+          )
+        })
+       }
        </div>
     </div>
   );
